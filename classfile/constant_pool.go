414d12ede45cc241d7eaedf3943fdd0cf037a70d
ryan.cao <ryan.cao@airparking.cn>
2018-04-19 14:18:23 +0800

    constant pool

--- classfile/constant_pool.go
+++ classfile/constant_pool.go
@@ -1,5 +1,11 @@
 package classfile
 
 // ConstantPool class file constant pool
-type ConstantPool struct {
+type ConstantPool []ConstantPoolInfo
+
+func readConstantPool(reader *ClassReader) ConstantPool {
+	cpCount := reader.readUint16()
+	for i := range cpCount - 1 {
+		
+	}
 }
