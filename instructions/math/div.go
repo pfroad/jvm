ddd131059547a2782c3e8720eefd44c58f607b51
Cao Yi <739822405@qq.com>
2018-05-01 21:23:32 +0800

    math instructions

---
+++ instructions/math/div.go
@@ -0,0 +1,55 @@
+package math
+
+import (
+	"jvm/instructions/common"
+	"jvm/runtime"
+)
+
+// add
+type IAdd struct {
+	common.NoOperandsInstruction
+}
+
+type LAdd struct {
+	common.NoOperandsInstruction
+}
+
+type FAdd struct {
+	common.NoOperandsInstruction
+}
+
+type DAdd struct {
+	common.NoOperandsInstruction
+}
+
+func (add *IAdd) Execute(frame runtime.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopInt()
+	v2 := stack.PopInt()
+	result := v1 + v2
+	stack.PushInt(result)
+}
+
+func (add *LAdd) Execute(frame runtime.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopLong()
+	v2 := stack.PopLong()
+	result := v1 + v2
+	stack.PushLong(result)
+}
+
+func (add *FAdd) Execute(frame runtime.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopFloat()
+	v2 := stack.PopFloat()
+	result := v1 + v2
+	stack.PushFloat(result)
+}
+
+func (add *DAdd) Execute(frame runtime.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopDouble()
+	v2 := stack.PopDouble()
+	result := v1 + v2
+	stack.PushDouble(result)
+}
---
+++ instructions/math/or.go
@@ -0,0 +1,28 @@
+package math
+
+import (
+	"jvm/instructions/common"
+	"jvm/runtime"
+)
+
+type IAnd struct {
+	common.NoOperandsInstruction
+}
+
+type LAnd struct {
+	common.NoOperandsInstruction
+}
+
+func (ia *IAnd) Execute(frame runtime.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopInt()
+	v2 := stack.PopInt()
+	stack.PushInt(v1 & v2)
+}
+
+func (la *LAnd) Execute(frame runtime.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopLong()
+	v2 := stack.PopLong()
+	stack.PushLong(v1 & v2)
+}
